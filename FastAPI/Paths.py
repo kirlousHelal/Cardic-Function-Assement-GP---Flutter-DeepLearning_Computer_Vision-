1e47aad242e5c09fe021c9d3560437b67a785345
Kirlous helal <73336168+kirlousHelal@users.noreply.github.com>
2024-06-23 23:53:51 +0300

    updated api and readme

---
+++ FastAPI/Paths.py
@@ -0,0 +1,16 @@
+# DataSet
+_dataRootPath = 'E:/FCIS Seniros - SC/GP - Full Year/EchoNet-Dynamic - DataSet'
+_videosPath = _dataRootPath + '/Videos'
+_fileNamesPath = _dataRootPath + '/FileList.csv'
+_volumeTracingPath = _dataRootPath + '/VolumeTracings.csv'
+
+# Loaded Videos
+_loadedVideosPath = ''
+
+# Transformer
+_transformerModelPath = 'TransformerModel.pt'
+
+# U-NET
+# _trueMasksPath = './True Masks'
+_ED_Model_Path = 'ED_Unet.weights.h5'
+_ES_Model_Path = 'ES_Unet.weights.h5'
